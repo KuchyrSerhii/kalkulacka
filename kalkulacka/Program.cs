d93a7f479f89be0eafa39c252bb7e2b93a4ff775
ou7pc18 <ou7pc18@OU7PC18>
2022-10-06 11:30:18 +0200

    Přidejte soubory projektu.

---
+++ kalkulacka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace kalkulacka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj jsem tvoje kalkulačka");
+            Console.WriteLine("Zadejte první číslo");
+            double prvni_cislo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Zadejte druhý číslo");
+            double druhy_cislo = Convert.ToDouble(Console.ReadLine());
+            double suma = prvni_cislo + druhy_cislo;
+            double rozdil = prvni_cislo - druhy_cislo;
+            double kraceni = prvni_cislo * druhy_cislo;
+            double deleni =  prvni_cislo / druhy_cislo;
+            Console.WriteLine(suma + " suma");
+            Console.WriteLine(rozdil + " rozdíl");
+            Console.WriteLine(kraceni + " kracení");
+            Console.WriteLine(deleni + " dělení");
+            Console.ReadKey();
+            
+        }
+    }
+}
